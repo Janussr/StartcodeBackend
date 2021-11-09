19f2e5888934072ff0b66fd960fc630ebe223745
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-09 14:08:54 +0100

    add Duck + Coffee DTO

---
+++ src/main/java/dtos/CoffeeDTO.java
@@ -0,0 +1,18 @@
+package dtos;
+
+public class CoffeeDTO {
+
+    private String file;
+
+    public CoffeeDTO(String file) {
+        this.file = file;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+}
---
+++ src/main/java/dtos/DuckDTO.java
@@ -0,0 +1,18 @@
+package dtos;
+
+public class DuckDTO {
+
+    private String url;
+
+    public DuckDTO(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
